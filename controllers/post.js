57b3167638ebe4e690f5713cf4f05a0ba46a9ffa
Kuddleman <edl1155@gmail.com>
2020-09-19 16:54:50 -0700

    build getPostsmethod in controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,14 +1,18 @@
 const Post = require('../models/post')
 
 exports.getPosts = ( req, res ) => {
-   const posts = Post.find()
+   const posts = Post.find().select("_id title body")
+   .then( posts => {
+     res.json({ posts })
+   })
+   .catch( err=>{ console.log(err) })
 }
 
 exports.createPost = ( req, res ) => {
   const post = new Post(req.body)
   //console.log("CREATING POST:", req.body)
   post.save().then( result => {
-    res.status(200).json({
+    res.json({
       post: result
     })
   } )
